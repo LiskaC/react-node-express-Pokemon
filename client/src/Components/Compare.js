35e44068b772afe44703d5fcbe7a6840e02b56a4
Liska Crofts <yscrofts@hotmail.com>
2020-05-18 22:57:02 +0200

    added handler functions to detect input change + submit

--- client/src/Components/Compare.js
+++ client/src/Components/Compare.js
@@ -3,12 +3,12 @@ import Card from './Card';
 import ComparisonResultsTable from './ResultsTable';
 
 //could not declare these variables inside the class Component - why?
-const message = <div className="compare-text">
+const Message = ({ onSubmit, onTextInput, pokemonNumber }) => <div className="compare-text">
   <p>Get me a random group of</p>
 
-  <form>
+  <form onSubmit={onSubmit}>
     <label>
-      <input type="number" name="number-of-pokemon"></input>
+      <input type="number" name="number-of-pokemon" onChange={onTextInput} value={pokemonNumber} />
       <br></br>
 1st Gen Pokemon, sorted in order of AWESOMENESS
 </label>
@@ -17,6 +17,7 @@ const message = <div className="compare-text">
     </div>
   </form>
 </div>
+//onsubmit
 
 const results = <ComparisonResultsTable />;
 
@@ -24,15 +25,31 @@ export default class Compare extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokemonNumber: []
+      pokemonNumber: 0
     }
+  };
+
+  handleOnSubmit = async (e) => {
+    e.preventDefault();
+    console.log(e);
+
+    //use state to pass in value to function
+    const response = await fetch("/express_backend");
+    console.log(response.json());
+  };
+
+  handleUserInput = (e) => {
+    this.setState({ pokemonNumber: e.target.value })
   };
 
   render() {
     return (
       <div>
         <Card
-          cardText={message}
+          cardText={<Message
+            onSubmit={this.handleOnSubmit}
+            onTextInput={this.handleUserInput}
+            pokemonNumber={this.state.pokemonNumber} />}
           resultsTable={results}
         />
       </div>
