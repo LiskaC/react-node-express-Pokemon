6ac2e50d20eae0973c38ba089dfa00ebd868eae0
Liska Crofts <yscrofts@hotmail.com>
2020-05-26 19:29:45 +0200

    getting a response from the server for strings hurrah

--- client/src/Components/Search.js
+++ client/src/Components/Search.js
@@ -41,24 +41,26 @@ export default class Search extends Component {
     e.preventDefault();
 
     const searchName = this.state.pokemonName;
-    const url = "http://localhost:5000/express_backend/";
+    const url = "http://localhost:5000/search_name/";
 
 
-    //one error is happening here : GET 500 internal error
+    //one error is happening here (line 48) : GET 500 internal error
     const inputResponse = await fetch(url + `${searchName}`);
 
     console.log('point1');
+
     inputResponse.json()
       .then(res => {
         //not entering into this function
+        //second error here : search:1 Uncaught (in promise) SyntaxError: 
+        //Unexpected token < in JSON at position 0
         this.setState({
           searchResults: res
         });
         console.log(this.state.searchResults);
 
-      });
-
-    setTimeout(() => console.log("point3"), 3000);
+      })
+      .catch(err => console.log(err));
   };
 
   render() {
